f7299f4986a35a18a59946469e2996f5c4fb9a37
Saker55 <faressaker666@gmail.com>
2025-11-27 17:17:18 +0200

    Update String1.py

--- src/methods/String1.py
+++ src/methods/String1.py
@@ -19,8 +19,43 @@ class String1:
         return String1(self.string  + " * " + other)
     
     def __rmul__(self, other):
-    # When doing string - object
+    # When doing string * object
         return String1(other  + " * " + self.string)
+    
+    def __truediv__(self, other):
+        # When doing a / b
+        if isinstance(other, String1):
+            return String1(self.string + " / " + other.string)
+        return String1(self.string  + " / " + other)
+    
+    def __rtruediv__(self, other):
+    # When doing string / object
+        return String1(other  + " / " + self.string)
+    
+    def __add__(self, other):
+        # When doing a + b
+        if isinstance(other, String1):
+            return String1(self.string + " + " + other.string)
+        return String1(self.string  + " + " + other)
+    
+    def __radd__(self, other):
+    # When doing string + object
+        return String1(other  + " + " + self.string)
+    
+    def __neg__(self):
+        # When doing -a
+        return String1( " - " + self.string)
+    
+    
+    def __floordiv__(self, other):
+        # When doing a // b
+        if isinstance(other, String1):
+            return String1(self.string + " // " + other.string)
+        return String1(self.string  + " // " + other)
+    
+    def __rfloordiv__(self, other):
+    # When doing string // object
+        return String1(other  + " // " + self.string)
     
     def __repr__(self):
         return f"{self.string}"
